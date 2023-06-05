ee283882072c56ca32c429a29ef605721323e4a1
John Den Hartog <jpdenhartog@madisoncollege.edu>
2023-06-05 18:40:41 -0500

    fix editcreature servlet address

--- src/main/java/jdh/controller/EditCreature.java
+++ src/main/java/jdh/controller/EditCreature.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(
-        urlPatterns = {"/deleteCreature"}
+        urlPatterns = {"/editCreature"}
 )
 public class EditCreature extends HttpServlet {
     @Override
